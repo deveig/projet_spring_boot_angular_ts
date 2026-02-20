2e93f89b13b00ef01f9981d15d12f8198de5ba42
Celine <cmdev@outlook.fr>
2026-02-20 17:16:05 +0100

    Update path to deploy

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,8 +2,8 @@ import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 
 export const routes: Routes = [
-  {
-    path: 'recipe-spring-angular',
-    component: AppComponent,
-  },
+//   {
+//     path: '**',
+//     component: AppComponent
+//   },
 ];
